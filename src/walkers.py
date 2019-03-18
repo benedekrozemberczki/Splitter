b9e234d85758d485ed24df2ba9ba337fdae61ef1
Benedek Rozemberczki <benedek.rozemberczki@gmail.com>
2019-03-18 11:29:39 +0000

    Update walkers.py

--- src/walkers.py
+++ src/walkers.py
@@ -19,7 +19,12 @@ class DeepWalker(object):
         self.graph = graph
         self.args = args
         
-    def small_walk(self,start_node):
+    def small_walk(self, start_node):
+        """
+        Doing a truncated random walk.
+        :param start_node: Start node for random walk.
+        :return walk: Truncated random walk with fixed maximal length.
+        """
         walk = [start_node]
         while len(walk) < self.args.walk_length:
             walk = walk + [random.sample(nx.neighbors(self.graph,walk[-1]),1)[0]]
