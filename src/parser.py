a2e9d71b7faaf646ce18aa7441fd9a5a52f378f2
Benedek Rozemberczki <benedek.rozemberczki@gmail.com>
2019-03-18 11:18:17 +0000

    Update parser.py

--- src/parser.py
+++ src/parser.py
@@ -51,7 +51,7 @@ def parameter_parser():
     parser.add_argument("--learning-rate",
                         type = float,
                         default = 0.025,
-	                help = "Learning rate. Default is 0.01.")
+	                help = "Learning rate. Default is 0.025.")
 
     parser.add_argument("--lambd",
                         type = float,
