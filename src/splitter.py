fe3ba0b334c8b8c703876e6debb9a57f7645ac4f
Benedek Rozemberczki <benedek.rozemberczki@gmail.com>
2019-12-17 17:42:35 +0000

    Add files via upload

--- src/splitter.py
+++ src/splitter.py
@@ -1,95 +1,107 @@
+"""Splitter Class."""
+
 import json
-import torch 
+import torch
 import random
 import numpy as np
 import pandas as pd
-from tqdm import tqdm, trange
+from tqdm import trange
 from walkers import DeepWalker
 from ego_splitting import EgoNetSplitter
 
 class Splitter(torch.nn.Module):
-     """
-     An implementation of "Splitter: Learning Node Representations that Capture Multiple Social Contexts" (WWW 2019).
-     Paper: http://epasto.org/papers/www2019splitter.pdf
-     """
-     def __init__(self, args, base_node_count, node_count):
-         """
-         Splitter set up.
-         :param args: Arguments object.
-         :param base_node_count: Number of nodes in the source graph.
-         :param node_count: Number of nodes in the persona graph.
-         """
-         super(Splitter, self).__init__()
-         self.args = args
-         self.base_node_count = base_node_count
-         self.node_count = node_count
+    """
+    An implementation of "Splitter: Learning Node Representations
+    that Capture Multiple Social Contexts" (WWW 2019).
+    Paper: http://epasto.org/papers/www2019splitter.pdf
+    """
+    def __init__(self, args, base_node_count, node_count):
+        """
+        Splitter set up.
+        :param args: Arguments object.
+        :param base_node_count: Number of nodes in the source graph.
+        :param node_count: Number of nodes in the persona graph.
+        """
+        super(Splitter, self).__init__()
+        self.args = args
+        self.base_node_count = base_node_count
+        self.node_count = node_count
 
-     def create_weights(self):
-         """
-         Creating weights for embedding.
-         """
-         self.base_node_embedding = torch.nn.Embedding(self.base_node_count, self.args.dimensions, padding_idx = 0)
-         self.node_embedding = torch.nn.Embedding(self.node_count, self.args.dimensions, padding_idx = 0)
-         self.node_noise_embedding = torch.nn.Embedding(self.node_count, self.args.dimensions, padding_idx = 0)
+    def create_weights(self):
+        """
+        Creating weights for embedding.
+        """
+        self.base_node_embedding = torch.nn.Embedding(self.base_node_count,
+                                                      self.args.dimensions,
+                                                      padding_idx=0)
 
-     def initialize_weights(self, base_node_embedding, mapping):
-         """
-         Using the base embedding and the persona mapping for initializing the embedding matrices.
-         :param base_node_embedding: Node embedding of the source graph.
-         :param mapping: Mapping of personas to nodes.
-         """
-         persona_embedding = np.array([base_node_embedding[original_node] for node, original_node in mapping.items()])
-         self.node_embedding.weight.data = torch.nn.Parameter(torch.Tensor(persona_embedding))
-         self.node_noise_embedding.weight.data = torch.nn.Parameter(torch.Tensor(persona_embedding))
-         self.base_node_embedding.weight.data = torch.nn.Parameter(torch.Tensor(base_node_embedding), requires_grad=False)
+        self.node_embedding = torch.nn.Embedding(self.node_count,
+                                                 self.args.dimensions,
+                                                 padding_idx=0)
 
-     def calculate_main_loss(self, sources, contexts, targets):
-         """
-         Calculating the main embedding loss.
-         :param sources: Source node vector.
-         :param contexts: Context node vector.
-         :param targets: Binary target vector.
-         :return main_loss: Loss value.
-         """
-         node_f = self.node_embedding(sources)
-         node_f = torch.nn.functional.normalize(node_f, p=2, dim=1)
-         feature_f = self.node_noise_embedding(contexts)
-         feature_f = torch.nn.functional.normalize(feature_f, p=2, dim=1)
-         scores = torch.sum(node_f*feature_f,dim=1)
-         scores = torch.sigmoid(scores)
-         main_loss = targets*torch.log(scores) + (1-targets)*torch.log(1-scores)
-         main_loss = -torch.mean(main_loss)
-         return main_loss
+        self.node_noise_embedding = torch.nn.Embedding(self.node_count,
+                                                       self.args.dimensions,
+                                                       padding_idx=0)
 
-     def calculate_regularization(self, pure_sources, personas):
-         """
-         Calculating the regularization loss.
-         :param pure_sources: Source nodes in persona graph.
-         :param personas: Context node vector.
-         :return regularization_loss: Loss value.
-         """
-         source_f = self.node_embedding(pure_sources)
-         original_f = self.base_node_embedding(personas)
-         scores = torch.clamp(torch.sum(source_f*original_f,dim=1),-15,15)
-         scores = torch.sigmoid(scores)
-         regularization_loss = -torch.mean(torch.log(scores))
-         return regularization_loss
+    def initialize_weights(self, base_node_embedding, mapping):
+        """
+        Using the base embedding and the persona mapping for initializing the embeddings.
+        :param base_node_embedding: Node embedding of the source graph.
+        :param mapping: Mapping of personas to nodes.
+        """
+        persona_embedding = np.array([base_node_embedding[n] for _, n in mapping.items()])
+        self.node_embedding.weight.data = torch.nn.Parameter(torch.Tensor(persona_embedding))
+        self.node_noise_embedding.weight.data = torch.nn.Parameter(torch.Tensor(persona_embedding))
+        self.base_node_embedding.weight.data = torch.nn.Parameter(torch.Tensor(base_node_embedding),
+                                                                  requires_grad=False)
 
-     def forward(self, sources, contexts, targets, personas, pure_sources):
-         """
-         Doing a forward pass.
-         :param sources: Source node vector.
-         :param contexts: Context node vector.
-         :param targets: Binary target vector.
-         :param pure_sources: Source nodes in persona graph.
-         :param personas: Context node vector.
-         :return loss: Loss value.
-         """
-         main_loss = self.calculate_main_loss(sources, contexts, targets)
-         regularization_loss = self.calculate_regularization(pure_sources, personas)
-         loss = main_loss + self.args.lambd*regularization_loss
-         return loss
-         
+    def calculate_main_loss(self, sources, contexts, targets):
+        """
+        Calculating the main embedding loss.
+        :param sources: Source node vector.
+        :param contexts: Context node vector.
+        :param targets: Binary target vector.
+        :return main_loss: Loss value.
+        """
+        node_f = self.node_embedding(sources)
+        node_f = torch.nn.functional.normalize(node_f, p=2, dim=1)
+        feature_f = self.node_noise_embedding(contexts)
+        feature_f = torch.nn.functional.normalize(feature_f, p=2, dim=1)
+        scores = torch.sum(node_f*feature_f, dim=1)
+        scores = torch.sigmoid(scores)
+        main_loss = targets*torch.log(scores)+(1-targets)*torch.log(1-scores)
+        main_loss = -torch.mean(main_loss)
+        return main_loss
+
+    def calculate_regularization(self, pure_sources, personas):
+        """
+        Calculating the regularization loss.
+        :param pure_sources: Source nodes in persona graph.
+        :param personas: Context node vector.
+        :return regularization_loss: Loss value.
+        """
+        source_f = self.node_embedding(pure_sources)
+        original_f = self.base_node_embedding(personas)
+        scores = torch.clamp(torch.sum(source_f*original_f, dim=1), -15, 15)
+        scores = torch.sigmoid(scores)
+        regularization_loss = -torch.mean(torch.log(scores))
+        return regularization_loss
+
+    def forward(self, sources, contexts, targets, personas, pure_sources):
+        """
+        Doing a forward pass.
+        :param sources: Source node vector.
+        :param contexts: Context node vector.
+        :param targets: Binary target vector.
+        :param pure_sources: Source nodes in persona graph.
+        :param personas: Context node vector.
+        :return loss: Loss value.
+        """
+        main_loss = self.calculate_main_loss(sources, contexts, targets)
+        regularization_loss = self.calculate_regularization(pure_sources, personas)
+        loss = main_loss + self.args.lambd*regularization_loss
+        return loss
+
 class SplitterTrainer(object):
     """
     Class for training a Splitter.
@@ -107,9 +119,11 @@ class SplitterTrainer(object):
         """
         Creating node noise distribution for negative sampling.
         """
-        self.downsampled_degrees = {node: int(1+self.egonet_splitter.persona_graph.degree(node)**0.75) for node in self.egonet_splitter.persona_graph.nodes()}
-        self.noises = [k for k,v in self.downsampled_degrees.items() for i in range(v)]
-          
+        self.downsampled_degrees = {}
+        for n in self.egonet_splitter.persona_graph.nodes():
+            self.downsampled_degrees[n] = int(1+self.egonet_splitter.persona_graph.degree(n)**0.75)
+        self.noises = [k for k, v in self.downsampled_degrees.items() for i in range(v)]
+
     def base_model_fit(self):
         """
         Fitting DeepWalk on base model.
@@ -141,7 +155,8 @@ class SplitterTrainer(object):
         persona_node_count = self.egonet_splitter.persona_graph.number_of_nodes()
         self.model = Splitter(self.args, base_node_count, persona_node_count)
         self.model.create_weights()
-        self.model.initialize_weights(self.base_node_embedding, self.egonet_splitter.personality_map)
+        self.model.initialize_weights(self.base_node_embedding,
+                                      self.egonet_splitter.personality_map)
         self.model = self.model.to(self.device)
 
     def transfer_batch(self, source_nodes, context_nodes, targets, persona_nodes, pure_source_nodes):
@@ -158,7 +173,8 @@ class SplitterTrainer(object):
         """
         Doing a weight update.
         """
-        loss = self.model(self.sources, self.contexts, self.targets, self.personas, self.pure_sources)
+        loss = self.model(self.sources, self.contexts,
+                          self.targets, self.personas, self.pure_sources)
         loss.backward()
         self.optimizer.step()
         self.optimizer.zero_grad()
@@ -166,7 +182,8 @@ class SplitterTrainer(object):
 
     def process_walk(self, walk):
         """
-        Process random walk (source, context) pairs. Sample negative instances and create persona node list.
+        Process random walk (source, context) pairs.
+        Sample negative instances and create persona node list.
         :param walk: Random walk sequence.
         """
         left_nodes = [walk[i] for i in range(len(walk)-self.args.window_size) for j in range(1, self.args.window_size+1)]
@@ -177,7 +194,8 @@ class SplitterTrainer(object):
         persona_nodes = np.array([self.egonet_splitter.personality_map[source_node] for source_node in source_nodes])
         pure_source_nodes = np.array(source_nodes)
         source_nodes = np.array((self.args.negative_samples+1)*source_nodes)
-        context_nodes = np.concatenate((np.array(context_nodes), np.random.choice(self.noises,node_pair_count*2*self.args.negative_samples)))
+        noises = np.random.choice(self.noises, node_pair_count*2*self.args.negative_samples)
+        context_nodes = np.concatenate((np.array(context_nodes), noises))
         positives = [1.0 for node in range(node_pair_count*2)]
         negatives = [0.0 for node in range(node_pair_count*self.args.negative_samples*2)]
         targets = np.array(positives + negatives)
@@ -191,7 +209,7 @@ class SplitterTrainer(object):
         self.cummulative_loss = self.cummulative_loss + loss_score
         self.steps = self.steps + 1
         average_loss = self.cummulative_loss/self.steps
-        self.walk_steps.set_description("Splitter (Loss=%g)" % round(average_loss,4))
+        self.walk_steps.set_description("Splitter (Loss=%g)" % round(average_loss, 4))
 
     def reset_average_loss(self, step):
         """
@@ -230,15 +248,16 @@ class SplitterTrainer(object):
         nodes = [node for node in self.egonet_splitter.persona_graph.nodes()]
         nodes.sort()
         nodes = torch.LongTensor(nodes).to(self.device)
-        self.embedding = self.model.node_embedding(nodes).cpu().detach().numpy()
+        embedding = self.model.node_embedding(nodes).cpu().detach().numpy()
         embedding_header = ["id"] + ["x_" + str(x) for x in range(self.args.dimensions)]
-        self.embedding  = np.concatenate([np.array(range(self.embedding.shape[0])).reshape(-1,1),self.embedding],axis=1)
-        self.embedding = pd.DataFrame(self.embedding, columns = embedding_header)
-        self.embedding.to_csv(self.args.embedding_output_path, index = None)
+        embedding = [np.array(range(embedding.shape[0])).reshape(-1, 1), embedding]
+        embedding = np.concatenate(embedding, axis=1)
+        embedding = pd.DataFrame(embedding, columns=embedding_header)
+        embedding.to_csv(self.args.embedding_output_path, index=None)
 
     def save_persona_graph_mapping(self):
         """
         Saving the persona map.
         """
         with open(self.args.persona_output_path, "w") as f:
-           json.dump(self.egonet_splitter.personality_map, f)                     
+            json.dump(self.egonet_splitter.personality_map, f)                     
